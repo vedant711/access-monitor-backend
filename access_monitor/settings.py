2cb66fe01698e4d01bfe7469382b5a824c8c9bd4
vedant711 <vedantdoshi711@gmail.com>
2023-02-27 18:24:23 +0530

    expire-session

--- access_monitor/settings.py
+++ access_monitor/settings.py
@@ -190,3 +190,5 @@ print(STATICFILES_DIRS)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
