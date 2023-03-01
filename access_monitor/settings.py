35a4a2119167dd65f65d61e7e4af42c8f17aaf2e
vedant711 <vedantdoshi711@gmail.com>
2023-03-01 10:59:10 +0530

    debug-off

--- access_monitor/settings.py
+++ access_monitor/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0w0tqyvbjg#)!#(v7#@7@!^%gov_$(sn*@)q4*)$49rxj%xe&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -186,7 +186,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'access/static'),
 ]
 
-print(STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
